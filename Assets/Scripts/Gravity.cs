ed4b8c95b2de1ee6e4f4dd5f9ba322d13c4bff36
DavEat <david.mestdagh@hotmail.com>
2020-01-31 21:51:28 +0100

    Add Gravity management

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public float gravityScale = -9.81f; 
+    public static Vector3 orientation = Vector3.down;
+
+    public Transform gravityTarget = null;
+
+    void FixedUpdate()
+    {
+        if (gravityTarget)
+            SetOrientation(gravityTarget);
+    }
+    public void SetOrientation(Transform transform)
+    {
+        orientation = transform.up * gravityScale;
+    }
+}
