127a29f8a5e0d08aa5fb8107e18b15176c61494e
helemaalbigt <van.bouwel.thomas@gmail.com>
2020-02-01 13:43:09 +0100

    NewComponents

---
+++ Assets/Scripts/Utilities/ReloadSceneAfterSeconds.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Test script that reloads scene every 7s seconds
+//use dto debug playarea
+public class ReloadSceneAfterSeconds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        Invoke("ReloadScene", 7f);
+    }
+
+    private void ReloadScene() {
+        Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+    }
+}
