8254df95219c0b8689d1eea79bb609b4ef556571
DavEat <david.mestdagh@hotmail.com>
2020-02-01 15:02:03 +0100

    Add sphere respawn

---
+++ Assets/Scripts/SphereCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCollision : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SphereKiller"))
+            SphereManager.inst.ReplaceSphere(transform);
+    }
+}
---
+++ Assets/Scripts/SphereManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SphereManager : Singleton<SphereManager>
+{
+    [Header("Sphere")]
+    [SerializeField] Transform startPosition;
+
+    public void ReplaceSphere(Transform sphere)
+    {
+        if (startPosition)
+        {
+            sphere.position = startPosition.position;
+            Rigidbody rb = sphere.GetComponent<Rigidbody>();
+            if (rb)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+        }
+    }
+}
