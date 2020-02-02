627c30942c1bc94b4ea15001205b1342b9a14ba7
helemaalbigt <van.bouwel.thomas@gmail.com>
2020-02-02 13:26:47 +0100

    add clouds and ground

---
+++ Assets/Scripts/FadeAndSwitchScenes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FadeAndSwitchScenes : MonoBehaviour
+{
+    public float timeUntilFadeAndSwitch;
+    public CameraFaderGroup faderGroup;
+    public GameManager gameManeger;
+
+    // Start is called before the first frame update
+    void Start() {
+        Invoke("_fadeAndSwitch", timeUntilFadeAndSwitch);
+    }
+
+    private void _fadeAndSwitch() {
+        StartCoroutine(FadeAndSwitch());
+    }
+
+    // Update is called once per frame
+    IEnumerator FadeAndSwitch() {
+        yield return faderGroup.FadeOut();
+        gameManeger.NextScene();
+    }
+}
