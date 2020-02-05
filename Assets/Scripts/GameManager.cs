929af3a0c5d512b1c6de7ff890140cc255a0151b
DavEat <david.mestdagh@hotmail.com>
2020-02-05 18:02:12 +0100

    Reload scene on A/X button press

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,17 @@ public class GameManager : MonoBehaviour
     public static GameManager inst;
     void Awake() { inst = this; }
 
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One) || OVRInput.GetDown(OVRInput.Button.Three))
+        {
+            ReloadScene();
+        }
+    }
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
     public void ChangeScene(int index)
     {
         SceneManager.LoadScene(index);
