ed1edb9b75a339e8448af7ce56d8c2fb4d0e75fc
DavEat <david.mestdagh@hotmail.com>
2020-02-05 11:17:54 +0100

    Fix rolling sound

--- Assets/Scripts/SFX/GrabbleEventSFX.cs
+++ Assets/Scripts/SFX/GrabbleEventSFX.cs
@@ -13,8 +13,9 @@ public class GrabbleEventSFX : MonoBehaviour
     Rigidbody m_rb = null;
     OVRGrabbableEvent m_grabbableEvent = null;
 
-    [SerializeField] float minVelocityToRing = .5f;
-    float m_speed;
+    [SerializeField] float m_rollingMaxSpeed = 1;
+    [SerializeField] AnimationCurve m_volumeCurve = null;
+    [SerializeField] AnimationCurve m_pitchCurve = null;
 
     void Start()
     {
@@ -23,51 +24,45 @@ public class GrabbleEventSFX : MonoBehaviour
 
         m_rb = GetComponent<Rigidbody>();
     }
-
-    void FixedUpdate()
+    void OnCollisionEnter(Collision collision)
     {
-        /*if (!m_grabbableEvent.isGrabbed && m_rb.velocity.sqrMagnitude < minVelocityToRing)
-        {
-            if (!m_rollSource.isPlaying)
-                m_rollSource.Play();
-        }
-        else
-        {
-            if (m_rollSource.isPlaying)
-                m_rollSource.Stop();
-        }*/
-
-        m_speed = m_rb.velocity.magnitude / minVelocityToRing;
+        if (m_rb.velocity.magnitude > .1f)
+            m_source.PlayOneShot(m_hitSound);
     }
+    void OnCollisionStay(Collision collision)
+    {
+        if (m_grabbableEvent.isGrabbed)
+            return;
 
+        var speed = m_rb.velocity.magnitude;
+        Debug.Log("speed= " + speed);
+
+        // normalize speed into 0-1
+        var scaledVelocity = Remap(Mathf.Clamp(speed, 0, m_rollingMaxSpeed), 0, m_rollingMaxSpeed, 0, 1);
+
+        // set volume based on volume curve
+        m_rollSource.volume = m_volumeCurve.Evaluate(scaledVelocity);
+
+        // set pitch based on pitch curve
+        m_rollSource.pitch = m_pitchCurve.Evaluate(scaledVelocity);
+
+        if (!m_rollSource.isPlaying)
+            m_rollSource.Play();
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (m_rollSource.isPlaying)
+        {
+            m_rollSource.Pause();
+        }
+    }
+    public float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
     public void GrabSound(OVRGrabbableEvent grabbable)
     {
         m_source.PlayOneShot(m_grabSound);
         m_rollSource.Pause();
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        m_source.PlayOneShot(m_hitSound);
-    }
-
-    void OnCollisionStay(Collision collision)
-    {
-        if (!m_grabbableEvent.isGrabbed && !m_rollSource.isPlaying && m_speed >= 0.1f /*&& collision.gameObject.tag == "Ground"*/)
-        {
-            m_rollSource.Play();
-        }
-        else if (!m_grabbableEvent.isGrabbed && m_rollSource.isPlaying && m_speed < 0.1f /*&& collision.gameObject.tag == "Ground"*/)
-        {
-            m_rollSource.Pause();
-        }
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        if (m_rollSource.isPlaying /*&& collision.gameObject.tag == "Ground"*/)
-        {
-            m_rollSource.Pause();
-        }
     }
 }
