07a4fab512a2b0560f2a9a7471e21a25107061e5
helemaalbigt <van.bouwel.thomas@gmail.com>
2020-02-06 11:15:49 +0100

    Fix static on earth; fix compresison artifacts on skybox; add gatherping on startposition

--- Assets/Scripts/PlayAreaView.cs
+++ Assets/Scripts/PlayAreaView.cs
@@ -11,10 +11,9 @@ public class PlayAreaView : MonoBehaviour
     {
         Vector3 bounds = OVRManager.boundary.GetDimensions(OVRBoundary.BoundaryType.PlayArea);
         _textMesh.text = Round(bounds.x) + "x" + Round(bounds.z) + "m";
-        bool bigEnough = bounds.x > 3f && bounds.z > 3f;
+        bool bigEnough = bounds.x >= 3f && bounds.z >= 3f;
         _textMesh.color = bigEnough ? _okCOlor : _smallColor;
         _playAreaSquare.localScale = new Vector3(bounds.x / 10f, bounds.z / 10f, 1);
-        _playAreaSquare.gameObject.SetActive(!bigEnough);
     }
 
     public float Round(float value) {
